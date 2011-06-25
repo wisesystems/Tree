3e43970412256973029826259e922ad478d9d6b4
Henry Smith <henry@henrysmith.org>
2011-06-25 02:10:34 +0100

    documentation

--- Exception/AutoloaderException.php
+++ Exception/AutoloaderException.php
@@ -19,9 +19,25 @@ use \Exception;
  */
 class AutoloaderException extends Exception {
 
-	const FILE_NOT_FOUND    = 0;
+	/**
+	 * Autoloader was unable to locate the class's PSR-0 filename anywhere in the
+	 * include_path. The most likely cause of this error is a mis-configured
+	 * include_path.
+	 */
+	const FILE_NOT_FOUND = 0;
+
+	/**
+	 * Autoloader found the file, but couldn't open it for reading. This is a
+	 * permissions problem.
+	 */
 	const FILE_NOT_READABLE = 1;
-	const CLASS_NOT_FOUND   = 2;
+
+	/**
+	 * The file was found and opened, but it did not contain the class it was
+	 * supposed to. This is most likely a result of a typo in the name of the
+	 * class being instantiated or in the declaration itself.
+	 */
+	const CLASS_NOT_FOUND = 2;
 
 }
 
--- Exception/ConfigurationException.php
+++ Exception/ConfigurationException.php
@@ -19,8 +19,22 @@ use \Exception;
  */
 class ConfigurationException extends Exception {
 
-	const FILE_NOT_FOUND     = 0;
-	const FILE_NOT_READABLE  = 1;
+	/**
+	 * The config file could not be found anywhere in the include_path. The most
+	 * likely cause of this problem is a mis-configured include_path.
+	 */
+	const FILE_NOT_FOUND = 0;
+
+	/**
+	 * The config file was found, but could not be opened for reading. This is a
+	 * permissions problem.
+	 */
+	const FILE_NOT_READABLE = 1;
+
+	/**
+	 * The config file was found and read, but its contents could not be parsed
+	 * as an ini file. The file needs to be checked manually for errors.
+	 */
 	const FILE_NOT_PARSEABLE = 2;
 
 }
