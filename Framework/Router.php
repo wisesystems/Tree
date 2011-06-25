c4d0bd70deb8bfff7b675338131df22b26ee28ac
Henry Smith <henry@henrysmith.org>
2011-06-25 03:29:58 +0100

    adds requestrouter

---
+++ Framework/Router.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace Tree\Framework;
+
+/**
+ * RequestRouter 
+ *
+ * Creates a two-way mapping between requests and Action subclasses
+ * 
+ * @author     Henry Smith <henry@henrysmith.org> 
+ * @copyright  2011 Henry Smith
+ * @license    GPLv2.0
+ * @package    Tree
+ * @subpackage Framework
+ * @version    0.00
+ */
+class Router {
+
+	/**
+	 * An array of route specifications of the following form:
+	 *      array(
+	 *              'request-pattern'    => '/article/{id}',
+	 *              'regular-expression' => '|^/article/(?P<id>[^/]+)$|',
+	 *              'action-id'          => 'ArticleView',
+	 *              'action-parameters'  => array(),
+	 *      )
+	 * 
+	 * @access private
+	 * @var    array
+	 */
+	private $routes = array();
+
+	/**
+	 * A string to prefix to URLs when generating request URLs
+	 * 
+	 * @access private
+	 * @var    string
+	 */
+	private $urlPrefix;
+
+	/**
+	 * Adds a route to the list of those to be considered when routing 
+	 * or generating requests
+	 * 
+	 * @access public
+	 * @param  string $requestPattern 
+	 * @param  string $actionId 
+	 * @param  array  $actionParameters [optional]
+	 */
+	public function addRoute($requestPattern, $actionId, array $actionParameters = array())
+	{
+		$regularExpression = $this->generateRegEx($requestPattern);
+		$patternParameters = $this->parsePattern($requestPattern);
+
+		$actionParameters = array_merge($patternParameters, $actionParameters);
+
+		$this->routes[] = array(
+			'request-pattern'    => $requestPattern,
+			'regular-expression' => $regularExpression,
+			'action-id'          => $actionId,
+			'action-parameters'  => $actionParameters,
+		);
+
+	}
+
+	/**
+	 * Maps a request path to an action 
+	 *
+	 * @access public
+	 * @param  string $requestPath
+	 * @return array
+	 */
+	public function getAction($requestPath)
+	{
+		$requestPath = trim($requestPath, '/');
+		$requestPath = '/' . $requestPath;
+
+		foreach ($this->routes as $route) {
+
+			$requestPattern    = $route['request-pattern'];
+			$regularExpression = $route['regular-expression'];
+			$actionId          = $route['action-id'];
+			$actionParameters  = $route['action-parameters'];
+
+			if (preg_match($regularExpression, $requestPath, $matches)) {
+
+				foreach ($matches as $name => $value) {
+					if (!is_int($name)) {
+						$actionParameters[$name] = $value;
+					}
+				}
+
+				return array($actionId, $actionParameters);
+
+			}
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * Maps an action to a request path 
+	 * 
+	 * @access public
+	 * @param  string $actionId 
+	 * @param  array  $actionParameters [optional]
+	 * @return string
+	 */
+	public function getPath($actionId, array $actionParameters = array())
+	{
+		$parameterList = array_keys($actionParameters);
+
+		foreach ($this->routes as $route) {
+
+			$routePattern    = $route['request-pattern'];
+			$routeActionId   = $route['action-id'];
+			$routeParameters = $route['action-parameters'];
+
+			if ($parameterList == array_keys($routeParameters)) {
+
+				$path = $routePattern;
+				$path = $this->injectParameters($path, $actionParameters);
+
+				return $path;
+			}
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * Maps an action to a request URL
+	 *
+	 * Requires a URL prefix to have been set using setUrlPrefix(), and
+	 * will throw an exception if this has not been done.
+	 * 
+	 * @access public
+	 * @param  string $actionId 
+	 * @param  array  $actionParameters [optional]
+	 * @return string
+	 */
+	public function getUrl($actionId, array $actionParameters = array())
+	{
+		if ($this->urlPrefix === null) {
+			throw new Exception('No URL prefix set');
+		}
+
+		$path = $this->getPath($actionId, $actionParameters);
+		if ($path === null) {
+			return null;
+		}
+
+		$url = $this->urlPrefix . $path;
+
+		return $url;
+	}
+
+	/**
+	 * Routes a request URL to an action 
+	 *
+	 * Requires a URL prefix to have been set.
+	 * 
+	 * @access public
+	 * @param  string $requestUrl 
+	 * @return array
+	 */
+	public function routeRequest($requestUrl)
+	{
+		if ($this->urlPrefix === null) {
+			throw new Exception('No URL prefix set');
+		}
+		
+		if (strpos($requestUrl, $this->urlPrefix) !== 0) {
+			return null;
+		}
+
+		$prefixLength = strlen($this->urlPrefix);
+		$requestPath  = substr($requestUrl, $prefixLength);
+
+		return $this->getAction($requestPath);
+	}
+
+	/**
+	 * Sets a prefix to be prepended to request paths for generating
+	 * request URLs in getUrl()
+	 * 
+	 * @access public
+	 * @param  string $urlPrefix 
+	 */
+	public function setUrlPrefix($urlPrefix)
+	{
+		$this->urlPrefix = $urlPrefix;
+	}
+
+	/**
+	 * Converts a request pattern into a regular expression
+	 *
+	 * Takes a human-readable request pattern such as '/article/{id}' and
+	 * converts it into a regular expression with named capture.
+	 * 
+	 * @access private
+	 * @param  string $requestPattern 
+	 * @return string
+	 */
+	private function generateRegEx($requestPattern)
+	{
+		$requestPattern = trim($requestPattern, '/');
+		$requestPattern = '/' . $requestPattern;
+
+		$regEx = preg_replace(
+			'|{([^}]+)}|',
+			'(?P<$1>[^/]+)',
+			$requestPattern
+		);
+
+		$regEx = "|^{$regEx}$|";
+
+		return $regEx;
+	}
+
+	/**
+	 * Inserts the given set of parameters into the given request pattern
+	 * to generate a request path
+	 *
+	 * For example, given a string '/article/{id}' and an array('id'=>123),
+	 * returns the string '/article/123'.
+	 * 
+	 * @access private
+	 * @param  string $requestPattern 
+	 * @param  array  $actionParameters 
+	 * @return string
+	 */
+	private function injectParameters($requestPattern, $actionParameters)
+	{
+		foreach ($actionParameters as $name => $value) {
+			$requestPattern = str_replace(
+				'{' . $name . '}',
+				$value,
+				$requestPattern
+			);
+		}
+		return $requestPattern;
+	}
+
+	/**
+	 * Returns an array listing the value names in the given request
+	 * pattern
+	 *
+	 * For example, given the pattern '/article/{id}', returns the array
+	 * [{ id : null}]
+	 * 
+	 * @access private
+	 * @param  string $requestPattern 
+	 * @return array
+	 */
+	private function parsePattern($requestPattern)
+	{
+		if (!preg_match_all('/\{([^{}]+)\}/', $requestPattern, $matches)) {
+			return array();
+		}
+
+		return array_fill_keys($matches[1], null);
+	}
+
+}
+
---
+++ Test/RouterTest.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace Tree\Test;
+
+require 'PHPUnit/Autoload.php';
+require '../Framework/Router.php';
+
+use \Tree\Framework\Router;
+use \PHPUnit_Framework_TestCase;
+use \ReflectionClass;
+
+/**
+ * RequestRouterTest 
+ *
+ * Tests the mapping of requests to actions by RequestRouter
+ * 
+ * @author     Henry Smith <henry@henrysmith.org> 
+ * @copyright  2010 - 2011 Henry Smith
+ * @license    GPLv2.0
+ * @package    Tree
+ * @subpackage Test
+ * @uses       \PHPUnit_Framework_TestCase
+ * @uses       \ReflectionClass
+ * @uses       \Tree\Framework\Router
+ * @version    0.00
+ */
+class RequestRouterTest extends PHPUnit_Framework_TestCase {
+
+	protected $router;
+	protected $reflection;
+
+	public function setUp()
+	{
+		$this->router     = new Router;
+		$this->reflection = new ReflectionClass('\Tree\Framework\Router');
+	}
+
+	/**
+	 * Tests that RequestRouter::generateRegEx creates well-formed regular
+	 * expressions to perform named-capture of the parameters in the
+	 * request path
+	 * 
+	 * @access public
+	 */
+	public function testGenerateRegEx()
+	{
+		$method = $this->reflection->getMethod('generateRegEx');
+		$method->setAccessible(true);
+
+		$examples = array(
+			array(
+				'input'  => '/test/',
+				'output' => '|^/test$|',
+			),
+			array(
+				'input'  => '/test/{id}',
+				'output' => '|^/test/(?P<id>[^/]+)$|',
+			),
+		);
+
+		foreach ($examples as $example) {
+
+			$input    = $example['input'];
+			$expected = $example['output'];
+			
+			$arguments = array($input);
+			$actual    = $method->invokeArgs($this->router, $arguments);
+
+			$this->assertEquals($actual, $expected);
+
+		}
+	}
+
+	/**
+	 * Tests that RequestRouter::parsePattern correctly extracts lists of
+	 * parameter names from request patterns
+	 * 
+	 * @access public
+	 */
+	public function testParsePattern()
+	{
+		$method = $this->reflection->getMethod('parsePattern');
+		$method->setAccessible(true);
+
+		$examples = array(
+			array(
+				'input'  => '/test/',
+				'output' => array(),
+			),
+			array(
+				'input'  => '/test/{id}',
+				'output' => array(
+					'id' => null,
+				),
+			),
+		);
+
+		foreach ($examples as $example) {
+
+			$input    = $example['input'];
+			$expected = $example['output'];
+			
+			$arguments = array($input);
+			$actual    = $method->invokeArgs($this->router, $arguments);
+
+			$this->assertEquals($actual, $expected);
+
+		}
+
+	}
+
+	/**
+	 * Tests that RequestRouter::injectParameters correctly injects
+	 * parameters into request patterns to generate request paths
+	 * 
+	 * @access public
+	 */
+	public function testInjectParameters()
+	{
+		$method = $this->reflection->getMethod('injectParameters');
+		$method->setAccessible(true);
+
+		$arguments = array(
+			'/example/path/{parameter}',
+			array(
+				'parameter' => 12345,
+			),
+		);
+
+		$path = $method->invokeArgs($this->router, $arguments);
+		
+		$this->assertEquals($path, '/example/path/12345');
+
+	}
+
+
+	/**
+	 * Tests that RequestRouter applies the right rules to requests and
+	 * extracts the parameters from those requests correctly
+	 * 
+	 * @access public
+	 */
+	public function testRoutesValidRequestsCorrectly()
+	{
+		$this->router->addRoute('/article/{id}', 'ArticleView');
+		$this->router->addRoute('/article/{id}/extra', 'ArticleView', array(
+			'extra' => true,
+		));
+
+		$route = $this->router->getAction('/article/12345');
+		$this->assertEquals($route[0], 'ArticleView');
+		$this->assertEquals($route[1], array(
+			'id' => '12345'
+		));
+
+		$route = $this->router->getAction('/article/12345/extra');
+		$this->assertEquals($route[0], 'ArticleView');
+		$this->assertEquals($route[1], array(
+			'id'    => '12345',
+			'extra' => true,
+		));
+	}
+
+	/**
+	 * Tests that RequestRouter doesn't inadvertantly route requests that
+	 * don't actually match any of the rules it has been given
+	 * 
+	 * @access public
+	 */
+	public function testRejectsInvalidRequests()
+	{
+		$this->router->addRoute('/test/{hi}', 'SomeAction');
+
+		$route = $this->router->getAction('/foo/bar/wut');
+
+		$this->assertEquals($route[0], null);
+		$this->assertEquals($route[1], null);
+	}
+
+	/**
+	 * Tests that RequestRouter::getPath returns paths that are correctly-
+	 * formed according to the specified request routes
+	 * 
+	 * @access public
+	 */
+	public function testGetPath()
+	{
+		$this->router->addRoute('/test/{hi}', 'SomeAction');
+		$this->router->addRoute('/test/{hi}/{foo}', 'OtherAction');
+
+		$path = $this->router->getPath('SomeAction', array('hi' => 'test'));
+
+		$this->assertEquals('/test/test', $path);
+	}
+
+	/**
+	 * Tests that RequestRouter::getPath and RequestRouter::getAction fit
+	 * together to create a two-way mapping between actions and patterns
+	 * 
+	 * @access public
+	 */
+	public function testTwoWayMapping()
+	{
+		$this->router->addRoute('/article/{id}', 'ArticleView');
+		$this->router->addRoute('/article/{id}/extra', 'ArticleView', array(
+			'extra' => true,
+		));
+		$this->router->addRoute('/test/{hi}', 'SomeAction');
+		$this->router->addRoute('/test/{hi}/{foo}', 'OtherAction');
+
+		$paths = array(
+			'/article/12345',
+			'/article/12345/extra',
+			'/test/whatever',
+			'/test/whatever/no',
+		);
+
+		foreach ($paths as $path) {
+			
+			$action0 = $this->router->getAction($path);
+			$path0   = $this->router->getPath($action0[0], $action0[1]);
+			$action1 = $this->router->getAction($path);
+			$path1   = $this->router->getPath($action1[0], $action1[1]);
+
+			$this->assertEquals($path, $path0);
+			$this->assertEquals($action0, $action1);
+			$this->assertEquals($path0, $path1);
+		}
+		
+	}
+
+	/**
+	 * Tests that setting a URL prefix allows us to give RequestRouter a
+	 * full request URL and expect it to parse out the part that actually
+	 * matters
+	 * 
+	 * @access public
+	 */
+	public function testUrlPrefix()
+	{
+		$this->router->setUrlPrefix('http://example.com');
+		$this->router->addRoute('/article/{id}', 'ArticleView');
+
+		$parameters = array(
+			'id' => 12345,
+		);
+		$url = $this->router->getUrl('ArticleView', $parameters);
+
+		$this->assertEquals('http://example.com/article/12345', $url);
+	}
+
+}
+
