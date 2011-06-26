81be67c9ab366acc3585d46ae427c999572773bb
Henry Smith <henry@henrysmith.org>
2011-06-26 03:58:32 +0100

    adds json response

---
+++ Response/Response/Json.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tree\Response;
+
+/**
+ * Response_Json 
+ *
+ * Handles sending JSON-encoded responses
+ * 
+ * @author     Henry Smith <henry@henrysmith.org> 
+ * @copyright  2010 Henry Smith
+ * @license    GPLv2.0
+ * @package    Tree
+ * @subpackage Response
+ * @version    0.00
+ */
+class Response_Json {
+
+	/**
+	 * Sets proper cache control headers to ensure that browsers always get fresh
+	 * JSON by default
+	 * 
+	 * @access public
+	 */
+	public function __construct()
+	{
+		$this->setHeader('Content-type', 'application/json');
+		$this->setheader('Pragma', 'no-cache');
+		$this->setHeader('Cache-Control', 'no-store, no-cache, max-age=0, must-revalidate');
+	}
+
+	/**
+	 * Sends the JSON response body
+	 * 
+	 * @access public
+	 */
+	public function sendResponseBody()
+	{
+		echo $this->responseBody;
+	}
+
+}
+
