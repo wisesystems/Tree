4b512474979d9f00bc3201aa6bf24b85de48fa31
Henry Smith <henry@henrysmith.org>
2011-07-12 00:26:23 +0100

    http response code handling

--- Exception/DatabaseException.php
+++ Exception/DatabaseException.php
@@ -4,8 +4,25 @@ namespace Tree\Exception;
 
 use \Exception;
 
+/**
+ * DatabaseException 
+ *
+ * Provides debug information about problems to do with databases
+ * 
+ * @author     Henry Smith <henry@henrysmith.org> 
+ * @copyright  2011 Henry Smith
+ * @license    GPLv2.0
+ * @package    Tree
+ * @subpackage Exception
+ * @uses       \Exception
+ * @version    0.00
+ */
 class DatabaseException extends Exception {
 
+	/**
+	 * Some database action requiring an open connection failed because the
+	 * connection could not be opened successfully.
+	 */
 	const CONNECTION_FAILED = 0;
 
 }
--- Response/Response.php
+++ Response/Response.php
@@ -2,6 +2,8 @@
 
 namespace Tree\Response;
 
+use \Exception;
+
 /**
  * Response 
  *
@@ -16,6 +18,66 @@ namespace Tree\Response;
  * @version    0.00
  */
 abstract class Response {
+
+	/**
+	 * An associative array of HTTP status codes and their associated messages
+	 *
+	 * @access private
+	 * @var    array
+	 * @static
+	 */
+	private static $statusCodes = array(
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+		207 => 'Multi-Status',
+		226 => 'IM Used',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		306 => 'Switch Proxy',
+		307 => 'Temporary Redirect',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		418 => "I'm a teapot",
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported',
+	);
+
+	/**
+	 * The HTTP status code of the response, e.g. 404, 500 etc
+	 * 
+	 * @access protected
+	 * @var    integer
+	 */
+	protected $responseStatus = 200;
 
 	/**
 	 * The body of the response, e.g. plain text, HTML, or binary image data
@@ -40,6 +102,7 @@ abstract class Response {
 	 */
 	public function sendResponse()
 	{
+		$this->sendStatusHeader();
 		$this->sendResponseHeaders();
 		$this->sendResponseBody();
 	}
@@ -62,13 +125,25 @@ abstract class Response {
 	 * @param  string $name 
 	 * @param  string $value 
 	 */
-	public function setHeader($name, $value = null)
+	public function setHeader($name, $value)
 	{
-		if (is_null($value)) {
-			$this->responseHeaders[] = $name;
-		} else {
-			$this->responseHeaders[$name] = $value;
+		$this->responseHeaders[$name] = $value;
+	}
+
+	/**
+	 * Sets the HTTP status code of the response
+	 * 
+	 * @access public
+	 * @param  integer $status 
+	 * @return string
+	 */
+	public function setStatus($status)
+	{
+		if (!isset(self::$statusCodes[$status])) {
+			throw new Exception('Unsupported HTTP status code');
 		}
+
+		$this->responseStatus = $status;
 	}
 
 	/**
@@ -79,12 +154,22 @@ abstract class Response {
 	private function sendResponseHeaders()
 	{
 		foreach ($this->responseHeaders as $name => $value) {
-			if (is_int($name)) {
-				header($value);
-			} else {
-				header("{$name}: $value");
-			}
+			header("{$name}: $value");
 		}
+	}
+
+	/**
+	 * Sends the HTTP status header of the response to the user
+	 * 
+	 * @access private
+	 */
+	private function sendStatusHeader()
+	{
+		$statusCode = $this->responseStatus;
+		$statusText = self::$statusCodes[$statusCode];
+
+		$statusHeader = "HTTP/1.1 {$statusCode} {$statusText}";
+		header($statusHeader);
 	}
 
 	/**
