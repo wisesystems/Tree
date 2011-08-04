0da63df735602761d1791a08d1ea07e689790fb1
Henry Smith <henry@henrysmith.org>
2011-08-04 21:41:46 +0100

    first steps towards adding the ORM

---
+++ Orm/Entity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tree\Orm;
+
+class Entity {
+
+	public function commitEntity()
+	{
+	}
+
+	public function deleteEntity()
+	{
+	}
+
+	public function revertEntity()
+	{
+	}
+
+	public function hydrateEntity($databaseRow)
+	{
+	}
+
+	public function setDatabaseConnection($connection)
+	{
+	}
+
+}
+
---
+++ Test/EntityTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tree\Test;
+
+require 'PHPUnit/Autoload.php';
+require '../Orm/Entity.php';
+
+use \Tree\Orm\Entity;
+use \PHPUnit_Framework_TestCase;
+
+class EntityTest extends PHPUnit_Framework_TestCase {
+
+	public function testSaveEntityInsertsNewRow()
+	{
+	}
+
+	public function testSaveEntityUpdatesExistingRow()
+	{
+	}
+
+	public function testDeleteEntityDeletesRow()
+	{
+	}
+
+	public function testRevertEntityRevertsChanges()
+	{
+	}
+
+	public function testHydrateEntityPopulatesEntity()
+	{
+	}
+
+}
+
