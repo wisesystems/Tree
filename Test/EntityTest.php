884b37bbe02b69e83e043d36eb875a5640795d09
Henry Smith <henry@henrysmith.org>
2011-08-06 20:32:33 +0100

    entity tests now fail properly

--- Test/EntityTest.php
+++ Test/EntityTest.php
@@ -4,6 +4,8 @@ namespace Tree\Test;
 
 require_once 'PHPUnit/Autoload.php';
 require_once 'PHPUnit/Extensions/Database/TestCase.php';
+require_once 'PHPUnit/Extensions/Database/DataSet/QueryDataSet.php';
+require_once '../Database/Connection.php';
 require_once '../Database/Connection/Pdo.php';
 require_once '../Orm/Entity.php';
 require_once 'Fake/Entity.php';
@@ -12,6 +14,7 @@ use \Tree\Database\Connection_Pdo;
 use \Tree\Orm\Entity;
 use \PHPUnit_Framework_TestCase;
 use \PHPUnit_Extensions_Database_TestCase;
+use \PHPUnit_Extensions_Database_DataSet_QueryDataSet;
 
 /**
  * EntityTest 
@@ -57,15 +60,45 @@ class EntityTest extends PHPUnit_Extensions_Database_TestCase {
 
 	public function testSaveEntityInsertsNewRow()
 	{
+		// todo: write code inserting new row
+		/*
+		e.g.:
+		$entity = new Fake_Entity($this->db);
+		$entity->title = 'this is something new';
+		$entity->body  = 'a whole new row right here';
+		$entity->saveEntity();
+		*/
 
+		$expected = $this->createFlatXMLDataSet('Data/entity-test-after-insert.xml');
+
+		$actual = new PHPUnit_Extensions_Database_DataSet_QueryDataSet($this->getConnection());
+		$actual->addTable('user');
+
+		$this->assertEquals($expected, $actual);
 	}
 
 	public function testSaveEntityUpdatesExistingRow()
 	{
+		// todo: write code updating existing row
+
+		$expected = $this->createFlatXMLDataSet('Data/entity-test-after-update.xml');
+
+		$actual = new PHPUnit_Extensions_Database_DataSet_QueryDataSet($this->getConnection());
+		$actual->addTable('user');
+
+		$this->assertEquals($expected, $actual);
 	}
 
 	public function testDeleteEntityDeletesRow()
 	{
+		// todo: write code deleting row
+
+		$expected = $this->createFlatXMLDataSet('Data/entity-test-after-delete.xml');
+
+		$actual = new PHPUnit_Extensions_Database_DataSet_QueryDataSet($this->getConnection());
+		$actual->addTable('user');
+
+		$this->assertEquals($expected, $actual);
 	}
 
 	public function testRevertEntityRevertsChanges()
