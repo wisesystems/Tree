018dcf883d43e3179cc0979f2cbb048dcaa6e445
Henry Smith <henry@henrysmith.org>
2011-09-12 02:01:02 +0100

    more test suite work

--- Test/Component/TestSuite.php
+++ Test/Component/TestSuite.php
@@ -26,10 +26,14 @@ class TestSuite extends PHPUnit_Framework_TestSuite {
 	{
 		$suite = new PHPUnit_Framework_TestSuite('PHPUnit Framework');
 
+		$suite->addTestSuite('\Tree\Test\Component\ActionTest');
+		$suite->addTestSuite('\Tree\Test\Component\PageTest');
+		$suite->addTestSuite('\Tree\Test\Component\TemplateTest');
 
 		return $suite;
 	}
 
 }
+
 
 
